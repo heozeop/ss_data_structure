95b529339fec19b4ec405f1526f39f151b24ceee
Lee seung chan <dltmdcks702@gmail.com>
2021-01-11 14:23:00 +0900

    test: prt

--- linked_list/linked_list/linkedList.c
+++ linked_list/linked_list/linkedList.c
@@ -8,21 +8,23 @@
 
 #include "linkedList.h"
 
-void linkedListExample(){
-    struct node head;
-    struct node a;
-    struct node b;
-    
-    head.next = &a;
-    a.val = 10;
-    b.val = 20;
-    a.next = &b;
-    b.next = NULL;
-    
-    struct node* start = &head;
-    while(start->next != NULL) {
-        start = start->next;
-        printf("next node value : %d\n", start->val);
-    }
-    return;
+void linkedListExample()
+{
+  struct node head;
+  struct node a;
+  struct node b;
+
+  head.next = &a;
+  a.val = 10;
+  b.val = 20;
+  a.next = &b;
+  b.next = NULL;
+
+  struct node *start = &head;
+  while (start->next != NULL)
+  {
+    start = start->next;
+    printf("next node value : %d\n", start->val);
+  }
+  return 0;
 }
